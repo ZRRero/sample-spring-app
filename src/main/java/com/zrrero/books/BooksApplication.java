ff5227d80c200eec6c9a7c5aaab81b8056885325
David Useche <david-use@hotmail.com>
2023-04-30 14:57:14 -0500

    Changed netty for jetty

--- src/main/java/com/zrrero/books/BooksApplication.java
+++ src/main/java/com/zrrero/books/BooksApplication.java
@@ -3,11 +3,9 @@ package com.zrrero.books;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
 @EnableJpaRepositories
-@EnableWebFlux
 public class BooksApplication {
 
     public static void main(String[] args) {
